104b6dee6d7412f73e3a8f083873f2a70e46674e
hata-junnosuke <jh19911209@icloud.com>
2023-04-16 15:06:58 +0900

    [add]フロント実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  //ログイン後のリダイレクト先を指定
+  const redirectUri = `${process.env["NEXT_PUBLIC_BASE_URL"]}/login`;
+  return (
+    <Auth0Provider
+      domain={process.env["NEXT_PUBLIC_AUTH0_DOMAIN"]!}
+      clientId={process.env["NEXT_PUBLIC_AUTH0_CLIENT_ID"]!}
+      audience={process.env["NEXT_PUBLIC_AUTH0_AUDIENCE"]!}
+      redirectUri={redirectUri}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
+
+export default MyApp;
